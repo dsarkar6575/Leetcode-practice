3b280e3e24e0da22873de6486718e94c39a955bf
Dipankar Sarkar <60103089+dsarkar6575@users.noreply.github.com>
2023-01-08 22:45:52 +0530

    Create 149. Max Points on a Line.java

---
+++ 149. Max Points on a Line.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int maxPoints(int[][] points) {
+        int n=points.length;
+        if(n<=2) return n;
+        int ans=2;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=i+1;j<n;j++)
+            {
+                int t=2;
+                for(int k=j+1;k<n;k++)
+                {
+                    int x = (points[j][1] - points[i][1]) * (points[k][0] - points[i][0]);
+                    int y = (points[k][1] - points[i][1]) * (points[j][0] - points[i][0]);
+                   if(x==y)t++;
+                }
+                if(t>ans)
+                ans=t;
+            }
+        }
+        return ans;
+    }
+}
